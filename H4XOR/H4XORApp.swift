f49ceba11f5c422e3321a5d82ad81a28da2db346
boughdiri-dorsaf <boughdiridorsaf12@gmail.com>
2022-12-23 17:37:15 +0100

    Initial Commit

---
+++ H4XOR/H4XORApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XORApp.swift
+//  H4XOR
+//
+//  Created by Boughdiri Dorsaf on 23/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct H4XORApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
